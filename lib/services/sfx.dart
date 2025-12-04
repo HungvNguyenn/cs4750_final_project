037142efab1bb85185b36a557178bdb65194898a
dakrow11 <drrowland@cop.edu>
2025-12-03 20:14:18 -0800

    Added sounds to grid puzzle

---
+++ lib/services/sfx.dart
@@ -0,0 +1,18 @@
+import 'package:audioplayers/audioplayers.dart';
+
+class Sfx {
+  static final AudioPlayer _player = AudioPlayer();
+
+  static Future<void> _play(String assetPath) async {
+    try {
+      await _player.stop();
+      await _player.play(AssetSource(assetPath));
+    } catch (e) {
+      print("Audio error: $e");
+    }
+  }
+
+  static Future<void> click()   => _play('sounds/click.wav');
+  static Future<void> correct() => _play('sounds/correct.wav');
+  static Future<void> reset()   => _play('sounds/reset.mp3');
+}
--- lib/word_search/word_search_page.dart
+++ lib/word_search/word_search_page.dart
@@ -4,6 +4,7 @@ import 'package:puzzle_game/pages/congratulation_page.dart';
 import 'dart:math';
 import 'package:puzzle_game/word_search/word_search_generator.dart';
 import 'package:puzzle_game/word_search/word_search_controller.dart';
+import '../services/sfx.dart';
 
 //Main word search page
 class WordSearchPage extends StatefulWidget {
